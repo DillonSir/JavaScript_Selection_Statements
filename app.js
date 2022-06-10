46104bdc1fe45371f0934de41cac8c4f4257146b
DillonSir <dillonsirmans1@gmail.com>
2022-06-09 22:58:53 -0400

    finished exercise

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ console.log(
 );
 
 // Exercise 1
-const num = 16;
+const num = 25;
 
 let userInput = window.prompt("Guess a number between 1 and 100:");
 
@@ -13,13 +13,12 @@ if (userInput < num) {
 } else if (userInput > num) {
   alert("Too high");
 } else {
-  alert("Congratulations. You guessed correctly!");
+  alert("Congratulations!!!. You guessed correctly!");
 }
 
 // Exercise 2
 let birthMonth = window
-  .prompt('Enter your birth month (e.g. "December"):')
-  .toLowerCase();
+  .prompt('Enter your birth month (e.g. "december"):');
 
 switch (birthMonth) {
   case "december":
@@ -43,5 +42,57 @@ switch (birthMonth) {
     alert("You were born in Fall.");
     break;
   default:
-    alert("You entered a invalid month.");
+    alert("I don't know that month.");
 }
+
+let typeId = "01";
+let colorId = "PU";
+let sizeId = "L";
+
+let type = "";
+let color = "";
+let size = "";
+
+switch(typeId) {
+  case "01":
+    type = "Tank Top";
+    break;
+  case "02":
+    type = "T-Shirt";
+  case "03":
+    type = "Long Sleeve";
+  case "04":
+    type = "Sweat Shirt";
+  default:
+    type = "Other";
+}
+
+switch(colorId) {
+  case "BL":
+    color = "Black";
+  case "BU":
+    color = "Blue";
+  case "RD":
+    color = "Red";
+  case "PU":
+    color = "Purple";
+    break;
+  default:
+    type = "White";
+}
+
+switch(sizeId) {
+  case "S":
+    size = "Small";
+  case "M":
+    size = "Medium";
+  case "L":
+    size = "Large";
+    break;
+  case "XL":
+    size = "Extra Large";
+  default:
+    size = "One size fits all";
+}
+
+console.log(`Product: ${size} ${color} ${type}`);
